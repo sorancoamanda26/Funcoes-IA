9e6c8b00c378bd6464a8ac1a6fbc6c3d0bc1fbbc
Amanda Leticia Soranço <107198852+sorancoamanda26@users.noreply.github.com>
2024-08-16 10:47:38 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
-
-
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultadp");
 
 
 
