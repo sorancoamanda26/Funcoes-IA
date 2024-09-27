f2db00acec08528c58d4da569a0e79d68157694b
Amanda Leticia Soranço <107198852+sorancoamanda26@users.noreply.github.com>
2024-09-27 09:27:17 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -2,30 +2,28 @@ const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
-const textoResultado = document.querySelector(".texto-resultadp");
-
-
+const textoResultado= document.querySelector(".texto-resultado");
 
 const perguntas = [
     {
-        enunciado: "Imagine que você tirou 30 dias de férias e pode escolher seu desino podendo levar quem quiser. Porém está em dúvida para onde ir, nós podemos te ajudar. Qual o clima de sua preferência?",
+        enunciado: " Imagine que você tirou 30 dias de férias e pode escolher seu destino podendo levar quem quiser. Porém, está com dúvida para onde ir, nós podemos te ajudar. Qual o clima de sua preferência? ",
         alternativas: [
             {
-                texto: " Lugares quentes ",
+                texto: " Lugares Quentes ",
                 afirmacao: " Lembre-se de levar protetor solar. ",
             },
             {
-                texto: " Lugares frios ",
+                texto: " Lugares Frios ",
                 afirmacao: " Lembre-se de levar agasalhos. ",
             }
         ]
     },
     {
-        enunciado: " Ótimo! Continuando com a sua preferência, o lugar que deseja ir, seria... ",
+        enunciado: " Ótimo! Continuando com sua preferência, o lugar que deseja ir, seria...?",
         alternativas: [
             {
                 texto: " Lugares agitados e movimentados ",
-                afirmacao: " Sempre fique atento, mantenha seus pertencer próximos. ",
+                afirmacao: " Sempre fique atento, mantenha seus pertences próximos. ",
             },
             {
                 texto: " Lugares calmos e aconchegantes ",
@@ -34,11 +32,11 @@ const perguntas = [
         ]
     },
     {
-        enunciado: " Maravilhoso! Me conta... quem irá te acompanhar?",
+        enunciado: " Maravilha! Me conte... Quem vai te acompanhar? ",
         alternativas: [
             {
-                texto: " Fámilia e/ou pet's ",
-                afirmacao: " Hotéis e resort's são a melhor opção para você. ",
+                texto: " Família e/ou pets ",
+                afirmacao: " Hotéis ou resort's são a melhor opção para você. ",
             },
             {
                 texto: " Amigos ",
@@ -47,28 +45,28 @@ const perguntas = [
         ]
     },
     {
-        enunciado: " Bom! De acordo com seu financeiro, quanto pretende gastar em sua viagem?",
+        enunciado: " Bom! De acordo com seu financeiro, quanto pretende gastar em sua viagem? ",
         alternativas: [
             {
                 texto: " Até três salários mínimos ",
                 afirmacao: " Boa opção para você são viagens nacionais. ",
             },
             {
-                texto: " Acima de três salários mínimos  ",
+                texto: " Acima de três salários mínimos ",
                 afirmacao: " Você irá adorar conhecer novos países. ",
             }
         ]
     },
     {
-        enunciado: " Para finalizar... Qual o meio de locomoção você pretende utilizar para chegar ao seu destino?",
+        enunciado: " Para Finalizar... Qual o meio de locomoção que você pretende utilizar para chegar ao seu destino? ",
         alternativas: [
             {
-                texto: " Carros ou motocicletas ",
-                afirmacao: " Seu destino pode ser próximo de você. ",
+                texto: " Carro ou Motocicletas ",
+                afirmacao: " Seu destino pode ser próximo a você. ",
             },
             {
-                texto: " Avião ou ônibus ",
-                afirmacao: " Sair de sua zona de conforto e conhecer novos horizontes. ",
+                texto: " Avião ou Ônibus ",
+                afirmacao: " Sair da sua zona de conforto e conhecer novos horizontes. ",
             }
         ]
     },
@@ -76,26 +74,39 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
-function mostraPergunta(){
+function mostraPergunta () {
+    if (atual>= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas();
 }
 
 function mostraAlternativas(){
     for (const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement("button");
+        const botaoAlternativas = document.createElement ("button");
         botaoAlternativas.textContent = alternativa.texto;
-        caixaAlternativas.appendChild(botaoAlternativas);
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+        caixaAlternativas.appendChild (botaoAlternativas);
     }
 }
 
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacao;
+    historiaFinal += afirmacoes + " ";
+    atual++;
+    mostraPergunta();
+}
 
-
-
-
-
-
+function mostraResultado(){
+    caixaPerguntas.textContent = "Seguindo a suas respostas em nossa pesquisa, a viagem ideal para você seria referente à essas sugestões:"
+    textoResultado.textContent = historiaFinal;
+    caixaAlternativas.textContent = "";
+}
 
 mostraPergunta();
